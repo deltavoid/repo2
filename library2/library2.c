490e4dc4c0cb1a0745e2cfcada0c919569641585
root <zhangqianyu.sys@bytedance.com>
2021-09-18 19:41:43 +0800

    repo2::library2 use repo1::library1

--- library2/library2.c
+++ library2/library2.c
@@ -1,4 +1,5 @@
 #include <repo2/library2/public/library2.h>
+#include <repo1/library1/public/library1.h>
 
 #include <stdio.h>
 
@@ -7,6 +8,9 @@
 
 void repo2_library2_display()
 {
-    printf("repo2_library2_display\n");
+    printf("repo2_library2_display: 1\n");
 
+    repo1_library1_display();
+
+    printf("repo2_library2_display: 2\n");
 }
